a35eeda908514708a9ccf48eab55329b1c8c2449
gueros <estefi_ma94@hotmail.com>
2015-07-06 03:46:02 -0500

    blabla

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
                             <div class="row">
                                 <div class="col-sm-9">
                                     <div class="texto-mision">
-                                        <p>oye eres mi sue&ntilde;o hecho realidad.</p>
+                                        <p>eres mi vida</p>
                                     </div>
                                 </div>
                                 <div class="col-sm-3">
